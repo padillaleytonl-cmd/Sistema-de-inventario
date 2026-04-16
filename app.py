ebe160cc97209685bed6e2a796aff6629745af52
padillaleytonl-cmd <padillaleytonl@gmail.com>
2026-04-16 00:57:47 -0400

    app.py

--- app.py
+++ app.py
@@ -356,5 +356,5 @@ def panel():
 # ---------------- RUN ----------------
 
 if __name__ == "__main__":
-import os
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
