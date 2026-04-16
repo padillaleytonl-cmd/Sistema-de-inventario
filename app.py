f30a840beab58af390c4d8d93fc273a8957dee9a
padillaleytonl-cmd <padillaleytonl@gmail.com>
2026-04-16 01:52:14 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -382,4 +382,5 @@ def panel():
 # ---------------- RUN ----------------
 
 if __name__ == "__main__":
-    if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
