4761d9e5858f35233340ec7a7e5288b0f9723973
padillaleytonl-cmd <padillaleytonl@gmail.com>
2026-06-03 01:35:13 -0400

    Add files via upload

--- facturacion/dtes/factura_compra.py
+++ facturacion/dtes/factura_compra.py
@@ -143,8 +143,8 @@ def generar_factura_compra_xml(
             f'<QtyItem>{_fmt_cantidad(qty)}</QtyItem>',
             f'<UnmdItem>{_escape_xml(unidad)}</UnmdItem>',
             f'<PrcItem>{_fmt_cantidad(prc)}</PrcItem>',
-            f'<MontoItem>{it["_monto_item"]}</MontoItem>',
             f'<CodImpAdic>{cod_imp_reten}</CodImpAdic>',
+            f'<MontoItem>{it["_monto_item"]}</MontoItem>',
         ]
         detalles_xml += '<Detalle>' + ''.join(linea_parts) + '</Detalle>'
 
--- facturacion/dtes/nota_debito.py
+++ facturacion/dtes/nota_debito.py
@@ -279,11 +279,11 @@ def generar_nota_debito_xml(
             linea_parts.append(f'<DescuentoMonto>{it["_desc_aplicado"]}</DescuentoMonto>')
         elif it.get('descuento_monto'):
             linea_parts.append(f'<DescuentoMonto>{it["_desc_aplicado"]}</DescuentoMonto>')
-        linea_parts.append(f'<MontoItem>{it["_monto_item"]}</MontoItem>')
         # ND asociada a factura de compra: CodImpAdic vincula el ítem con la
-        # retención del encabezado (igual que LibreDTE). Solo en líneas afectas.
+        # retención. Va ANTES de MontoItem (orden del schema). Solo líneas afectas.
         if impto_reten and not es_exe:
             linea_parts.append(f'<CodImpAdic>{impto_reten.get("tipo_imp", 15)}</CodImpAdic>')
+        linea_parts.append(f'<MontoItem>{it["_monto_item"]}</MontoItem>')
         detalles_xml += '<Detalle>' + ''.join(linea_parts) + '</Detalle>'
 
     # 6. Descuento global
