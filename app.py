caa6d9d506876226b447872f86066eaefba71086
padillaleytonl-cmd <padillaleytonl@gmail.com>
2026-06-04 10:23:07 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -26954,7 +26954,7 @@ def admin_lusync_sii_test_set_liquidacion():
                 ]),
                 # CASO 4
                 dict(items=[
-                    {'nombre': 'NETO ANTICIPO FACTURACION', 'cantidad': 299, 'monto': 550000, 'exento': False, 'tpo_doc_liq': 33},
+                    {'nombre': 'NETO ANTICIPO FACTURACION', 'cantidad': 299, 'monto': 550000, 'exento': False, 'tpo_doc_liq': 30},
                     {'nombre': 'NETO FACTURAS', 'cantidad': 51, 'monto': 353979, 'exento': False, 'tpo_doc_liq': 30},
                     {'nombre': 'EXENTO FACTURAS', 'cantidad': 57, 'monto': 208950, 'exento': True, 'tpo_doc_liq': 30},
                     {'nombre': 'NETO FACTURAS ELECTRONICAS', 'cantidad': 44, 'monto': 106363, 'exento': False, 'tpo_doc_liq': 33},
