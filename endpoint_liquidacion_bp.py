2accf72952b2f15179b97ddcbe37dd18bf4ffd59
padillaleytonl-cmd <padillaleytonl@gmail.com>
2026-06-13 23:54:04 -0400

    Add files via upload

--- endpoint_liquidacion_bp.py
+++ endpoint_liquidacion_bp.py
@@ -163,7 +163,7 @@ def admin_lusync_sii_test_set_liquidacion():
                     {'nombre': 'EXENTO FACTURA ELECTRÓNICA 4768', 'cantidad': 1, 'monto': 365464, 'exento': True, 'tpo_doc_liq': 33},
                     {'nombre': 'NETO NOTA DE CRÉDITO 328', 'cantidad': 1, 'monto': -50894, 'exento': False, 'tpo_doc_liq': 60},
                     {'nombre': 'EXENTO NOTA DE CRÉDITO 328', 'cantidad': 1, 'monto': -18006, 'exento': True, 'tpo_doc_liq': 60},
-                    {'nombre': 'BOLETAS', 'cantidad': 1, 'monto': 6228679, 'exento': False, 'tpo_doc_liq': 39, 'prc_item': 6228679},
+                    {'nombre': 'BOLETAS', 'cantidad': 8262, 'monto': 6228679, 'exento': False, 'tpo_doc_liq': 39, 'prc_item': 753.89482},
                 ], comisiones=None),
                 # CASO 3 — glosas con acentos exactas
                 dict(items=[
